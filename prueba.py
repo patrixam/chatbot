d7ad260fcf9027a6bc65cc6fa6dc13c8f2546dda
Patricia Arroyo <143134385+patrixam@users.noreply.github.com>
2025-02-05 18:47:42 +0100

    Create prueba.py

---
+++ prueba.py
@@ -0,0 +1,12 @@
+#BOTONES:
+if st.button("Haz clic aquí"):
+   st.write("¡Botón presionado!")
+
+#ENTRADA DE TEXTO:
+name = st.text_input("¿Cómo te llamas?")
+if name:
+    st.write(f"Hola, {name}!")
+
+#DESLIZADORES:
+age = st.slider("Selecciona tu edad", 0, 100, 25)
+st.write(f"Tienes {age} años")
